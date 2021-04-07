a9d888eb5a0237123f11db128563b8f06540e117
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2021-04-07 18:07:58 +0530

    Add files via upload

---
+++ commands/say.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+module.exports.run = async(client,message,args)=>{
+  const messageToSay = args.join(" ");
+  const sayEmbed = new Discord.MessageEmbed()
+    .setTitle(`${message.author.tag} said: ${messageToSay}`)
+    .setFooter(message.author.tag, message.author.displayAvatarURL())
+	.setColor('BLUE')
+	.setTimestamp();
+  try {
+	  await message.channel.send(sayEmbed);
+	} catch (err) {
+		console.log(err);
+		message.channel.send("I am not able to say that message!!");
+	}
+      
+}
+
+module.exports.help ={
+    name:"say"
+}
