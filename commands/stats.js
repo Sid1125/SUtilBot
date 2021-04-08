636871ea491cfbb8474b88a4e108502b4adc55e3
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2021-04-08 16:47:33 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 module.exports.run = async(client,message,args)=>{
 const statsEmbed = new Discord.MessageEmbed()
     .setTitle(`Bot Stats`)
- .setDescription(`Server count: ${client.guilds.cache.size}\nPing: **${client.ws.ping}ms**\nRobot connected in **${client.voice.connections.size}** channels !`)
+ .setDescription(`Server count: ${client.guilds.cache.size}\nPing: **${client.ws.ping}ms**\nRobot connected in **${client.voice.connections.size}** channels !\nUser Count: ${client.users.cache.size}`)
     .setColor('BLUE')
     .setTimestamp();
 
