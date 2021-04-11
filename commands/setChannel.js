4387d02a8d12dc3f205db45a324f4f0b07d32551
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2021-04-11 16:59:54 +0530

    Add files via upload

---
+++ commands/setChannel.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Schema = require('../models/welcomeChannel');
+
+module.exports.run = async(client,message,args)=>{
+ if(!message.member.hasPermission('ADMINISTRATOR')) return;
+
+ let channel = message.mentions.channels.first();
+ if(!channel) return message.reply('Please mention a channel to set it as a Welcome channel!!');
+
+ Schema.findOne({ Guild: message.guild.id }, async(err, data) => {
+     if(data){
+        data.Channel = channel.id;
+        data.save();
+     } else {
+        new Schema({
+            Guild: message.guild.id,
+            Channel: channel.id,
+        }).save();
+     }
+     message.reply(`${channel} has been set as the new Welcome Channel`);
+ })
+}
+
+module.exports.help ={
+    name:"swc"
+}
