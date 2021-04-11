85d35bbd725d539408deb8e5b623f53ec7ca06ec
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2021-04-11 16:55:15 +0530

    Add files via upload

---
+++ commands/meme.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const got = require('got') // npm install got
+
+  module.exports.run = async (client, message, args) => {
+
+            got('https://www.reddit.com/r/memes/random/.json').then(res => {
+                let content = JSON.parse(res.body)
+                message.channel.send(
+                    new Discord.MessageEmbed()
+                        .setTitle(content[0].data.children[0].data.title)     
+                        .setImage(content[0].data.children[0].data.url)
+                        .setColor("RANDOM")
+                        .setFooter(`👍 ${content[0].data.children[0].data.ups} | 👎 ${content[0].data.children[0].data.downs} | 💬 ${content[0].data.children[0].data.num_comments}`)
+                )
+            })
+        }
+    
+
+module.exports.help = { // Update according to your handler. lol
+    name: "meme",
+    }
