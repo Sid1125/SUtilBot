24d5edf64ed6af273a2fe9c1810b7ec023d022ce
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2021-04-13 15:25:06 +0530

    Update guild.js

--- commands/guild.js
+++ commands/guild.js
@@ -2,7 +2,7 @@ const { Discord, Client, MessageEmbed } = require('discord.js')
 module.exports.run = async(client, message, args) =>{
    let serverlist = ''
 
-if{!message.author.tag == '699257426712592385'} return;
+if(!message.author.tag == '699257426712592385') return;
         client.guilds.cache.forEach((guild) => {
             serverlist = serverlist.concat( "** - **" + guild.name + "| ID: " + guild.id + `| Members:`+ guild.memberCount + "\n"  )
         })
