934b99b9d6cd841038a46f08950d160e822e5421
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2021-04-13 15:19:53 +0530

    Create guild.js

---
+++ commands/guild.js
@@ -0,0 +1,18 @@
+const { Discord, Client, MessageEmbed } = require('discord.js') 
+module.exports.run = async(client, message, args) =>{
+   let serverlist = ''
+
+if{!message.author.tag == '699257426712592385'} return;
+        client.guilds.cache.forEach((guild) => {
+            serverlist = serverlist.concat( "** - **" + guild.name + "| ID: " + guild.id + `| Members:`+ guild.memberCount + "\n"  )
+        })
+    
+        const embed = new MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle(`Servers that have  Bot`, '')
+        .setDescription(serverlist)
+        message.channel.send({embed});
+  }
+modul.exports.help ={
+    name = "guilds",
+}
