87bc20dffe6e71d5a1cf11474e6ee95b5080bbc3
Siddharth Sinha <siddharthsinha1125@gmail.com>
2021-04-14 10:20:44 +0530

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -16,11 +16,12 @@ module.exports = {
                     
                     
                     fields: [
-                        { name: 'Bot', value: '`debug`, `ping`' },
+                        { name: 'Bot', value: '`debug`, `ping`, `covid`, `swc(set a welcome channel)`, `cwc(check welcome channel)`' },
 						{ name: 'No prefix', value: '`dream`, `technoblade`, `wilbursoot`, `bbs`, `rasode me kon tha`, `drista`, `RIP`' },
 						{ name: 'Roleplay (no prefix)', value: '`lets fight`, `no u`, `ghostbot`, `so what`' },
 			    { name: 'Image', value: '`av`, `delete`, `jail`, `trigger`, `hitler`, `wanted`' },
 			    { name: 'Moderation', value: '`ban`, `kick`, `nuke`' },
+                { name: 'Fun', value: '`mcachieve`, `meme`, `minecraft`, `anime`, `fight`' },
 			    { name: 'Giveaway - Do s#startgive #channel duration winner_count prize', value: '`startgive`, `endgiveaway`, `rgiveaway(r = reroll)`' },
                         { name: 'Music(CAUTION - only do s#play song name[no links])', value: music },
                         { name: 'Filters', value: '`bassboost`, `8D`, `vaporwave`, `nightcore`, `phaser`, `tremolo`, `vibrato`, `reverse`, `treble`, `normalizer`, `surrounding`, `pulsator`, `subboost`, `karaoke`, `flanger`, `gate`, `haas`, `mcompand`' },
