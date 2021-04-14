700a1d2f21e3f36ceb59e429e263f1709f33e5e9
Siddharth Sinha <siddharthsinha1125@gmail.com>
2021-04-14 12:13:03 +0530

    jfdkufgklgthdipi;ugpiyfu;

--- commands/addrole.js
+++ commands/addrole.js
@@ -1,15 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Message } = require('discord.js')
 
 
-
-module.exports.run = async(client,message,args)=>{
-//!addrole @user @role
- if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry Pal! You don't have the correct Perms right now! Permissions Needed:- MANAGE_MEMBERS");
-
-
-}
-
-module.exports.help ={
-    name:"addrole"
-}
+    module,exports.run = async(client, message, args) => {
+       
+        //so firstly we will check whether the author of the message has permissions
+        //this line means if the author doesn't have manage roles permission it will stop the process and send the following text
+        if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('You do not have permission.')
+        //next we define some variables
+        const target = message.mentions.members.first() //member = mentions
+        if(!target) return message.channel.send('No member specified') //when no member is pinged
+        const role = message.mentions.roles.first() // roles = mentions
+        if(!role) return message.channel.send('No role specified') //when no role is specified or pinged
+        //now the code!
+        await target.roles.add(role) // adding the role to the user
+        message.channel.send(`${target.user.username} has obtained a role`)
+    }
+    module.exports = {
+        name : 'addrole',
+    }
