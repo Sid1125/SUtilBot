74ac1a6ba7b3cff99236854c4cdce1c622ec9283
Siddharth Sinha <siddharthsinha1125@gmail.com>
2021-06-04 09:35:41 +0530

    dghdfjgkhjgkd

--- commands/ban.js
+++ commands/ban.js
@@ -5,9 +5,10 @@ const client = new Discord.Client();
 module.exports.run = async(client,message,args)=>{
 
 if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Invalid Permissions")
+if (!message.guild.me.hasPermission(`BAN_MEMBERS`)) return message.reply(`I am sorry, but I don't have the perms to ban a user.`);
 let User = message.guild.member(message.mentions.users.first())
 if (!User) return message.channel.send("Invalid User")
-
+if (User.hasPermission(`ADMINISTRATOR`)) return message.reply(`I am sorry, but I can not ban ${User} as he/she is an Admin.`);
 let banReason = args.join(" ").slice(22);
 if (!banReason) {
   banReason = "None"
--- commands/kick.js
+++ commands/kick.js
@@ -5,9 +5,10 @@ const client = new Discord.Client();
 module.exports.run = async(client,message,args)=>{
 
 if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Invalid Permissions")
+if (!message.guild.me.hasPermission(`KICK_MEMBERS`)) return message.reply(`I am sorry, but I don't have the perms to ban a user.`);
 let User = message.guild.member(message.mentions.users.first())
 if (!User) return message.channel.send("Invalid User")
-
+if (User.hasPermission(`ADMINISTRATOR`)) return message.reply(`I am sorry, but I can not kick ${User} as he/she is an Admin.`);
 let kickReason = args.join(" ").slice(22);
 if (!kickReason) {
   kickReason = "None"
