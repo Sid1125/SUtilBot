92d955c4798334933fd75d9cbf3637c883e4662d
Sid1125 <73689159+Sid1125@users.noreply.github.com>
2022-07-17 14:32:36 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -234,7 +234,7 @@ for (const file of events) {
 for (const file of player) {
     console.log(`Loading discord-player event ${file}`);
     const event = require(`./player/${file}`);
-    client.player.on(file.split(".")[0], event.bind(null, client));
+    client.on(file.split(".")[0], event.bind(null, client));
 };
 
 
