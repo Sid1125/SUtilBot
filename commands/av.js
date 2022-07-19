cbc2e8d2260233e5648415339055cc0435bd3739
Siddharth Sinha <siddharthsinha1125@gmail.com>
2022-07-19 20:03:48 +0530

    Signed-off-by: Siddharth Sinha <siddharthsinha1125@gmail.com>

--- commands/av.js
+++ commands/av.js
@@ -9,6 +9,14 @@ module.exports.run = async(client,message,args)=>{
 
   let avatar = ad.displayAvatarURL({ dynamic: true, format: 'png', });
   let attachment = new Discord.MessageAttachment(avatar);
+  const embed = new Discord.MessageEmbed()
+  .setTitle(`You look Cool Today!`)     
+  .setImage(avatar)
+  .setColor("BLUE")
+  .setDescription('Avatar')
+
+channel.send({ embeds: [embed], files: [attachment] })
+
     message.channel.send(attachment)
     message.channel.send({content: "You look Cool Man!!"})
 }
